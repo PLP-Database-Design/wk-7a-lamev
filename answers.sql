a6f13c3482ee182a0d7c4bf51ad0a185324a2a0f
lamev <okothlamek@gmail.com>
2025-04-18 13:50:16 +0300

    database wk 7 assingment submitted

---
+++ answers.sql
@@ -0,0 +1,47 @@
+-- Create normalized table
+CREATE TABLE OrderProducts_1NF (
+    OrderID INT,
+    CustomerName VARCHAR(50),
+    Product VARCHAR(50)
+);
+
+-- Insert split data
+INSERT INTO OrderProducts_1NF (OrderID, CustomerName, Product)
+VALUES
+    (101, 'John Doe', 'Laptop'),
+    (101, 'John Doe', 'Mouse'),
+    (102, 'Jane Smith', 'Tablet'),
+    (102, 'Jane Smith', 'Keyboard'),
+    (102, 'Jane Smith', 'Mouse'),
+    (103, 'Emily Clark', 'Phone');
+
+    -- Orders table
+CREATE TABLE Orders_2NF (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(50)
+);
+
+-- Order items table
+CREATE TABLE OrderItems_2NF (
+    OrderID INT,
+    Product VARCHAR(50),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders_2NF(OrderID)
+);
+
+-- Insert split data
+INSERT INTO Orders_2NF (OrderID, CustomerName)
+VALUES
+    (101, 'John Doe'),
+    (102, 'Jane Smith'),
+    (103, 'Emily Clark');
+
+INSERT INTO OrderItems_2NF (OrderID, Product, Quantity)
+VALUES
+    (101, 'Laptop', 2),
+    (101, 'Mouse', 1),
+    (102, 'Tablet', 3),
+    (102, 'Keyboard', 1),
+    (102, 'Mouse', 2),
+    (103, 'Phone', 1);
